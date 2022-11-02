879881e23a11252684a7693cb3fb183f4b54156f
Mohamed Ahmed Fouad <moodymohamed798@gmail.com>
2022-11-02 23:22:37 +0200

    2nd commit

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-let subscribed = true;
-let loggedIn = true;
-if (subscribed === true) {
-  console.log("show the video");
-} else if (loggedIn === true) {
-  console.log("tell the user to upgrade their subscription");
-} else {
-  console.log("tell user to log into account");
-}
-let cash = 50;
-let price = 40;
-let defrence = cash - price;
-let IsStoreOpen = false;
-if (cash > price) {
-  console.log(`you paid extra - here's ${defrence} dollars change`);
-} else if (cash === price) {
-  console.log("you paid the exact amount, have a nice day!");
-} else {
-  console.log(`not enouch money - you stll owe ${-defrence} dollars`);
-}
-if (cash >= price && !IsStoreOpen) {
-  console.log("print the receipt");
+for (let i = 1; i <= 20; i++) {
+    if (i%3===0&&i%5!=0) {
+        console.log(`${i} -> Frontend`);
+    }
+    else if (i%5===0&&i%3!=0) {
+        console.log(`${i} -> Simplified`);
+    }
+    else if (i%3===0&&i%5===0) {
+        console.log(`${i} -> Frontend Simplified`);
+    }
+   else{
+    console.log(`${i} -> ${i}`);
+   }
 }
