30394b9c0a83110b05210f1120feeb6e32affa3c
Mohamed Ahmed Fouad <moodymohamed798@gmail.com>
2022-11-12 20:03:41 +0200

    functions

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-for (let i = 1; i <= 20; i++) {
-    if (i%3===0&&i%5!=0) {
-        console.log(`${i} -> Frontend`);
-    }
-    else if (i%5===0&&i%3!=0) {
-        console.log(`${i} -> Simplified`);
-    }
-    else if (i%3===0&&i%5===0) {
-        console.log(`${i} -> Frontend Simplified`);
-    }
-   else{
-    console.log(`${i} -> ${i}`);
-   }
-}
+let CToF = (C) => {
+  console.log(C * 1.8 + 32);
+};
+CToF(1);
+CToF(10);
+CToF(30);
