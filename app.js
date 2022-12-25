07d0e1343b49b96077b9b5d20bc6ada2a155db0e
Mohamed Ahmed Fouad <moodymohamed798@gmail.com>
2022-12-25 14:56:22 +0200

    DOM

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-let users = [
-   { name: "mohamed", email: "moodymohamed798@gmail.com", password: "123" },
-   { name: "ahmed", email: "ahmed@gmail.com", password: "ahmed" }
-];
-function register(user) {
-users.push(user)   
+function toggleDarkMode() {
+document.querySelector('body').classList.toggle('dark-theme');
 }
-register({name: "toty", email: "toty@gmail.com", password: "toty"} )
-console.log(users);
